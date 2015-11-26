d9e9a9ee5de9cf0fbf0153274b6ce7eaf8b5edd0
Jack Jiang <jb2011@163.com>
2015-11-26 12:01:36 +0800

    Update server demo header docs.

--- src_all/server/MobileIMSDKServer/src/net/openmob/mobileimsdk/server/demo/MessageQoSEventS2CListnerImpl.java
+++ src_all/server/MobileIMSDKServer/src/net/openmob/mobileimsdk/server/demo/MessageQoSEventS2CListnerImpl.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2015 Jack Jiang The MobileIMSDK Project. 
+ * All rights reserved.
+ * Project URL:https://github.com/JackJiang2011/MobileIMSDK
+ *  
+ * openmob.net PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 
+ * MessageQoSEventS2CListnerImpl.java at 2015-11-26 12:00:12, code by Jack Jiang.
+ * You can contact author with jack.jiang@openmob.net or jb2011@163.com.
+ */
 package net.openmob.mobileimsdk.server.demo;
 
 import java.util.ArrayList;
--- src_all/server/MobileIMSDKServer/src/net/openmob/mobileimsdk/server/demo/ServerEventListenerImpl.java
+++ src_all/server/MobileIMSDKServer/src/net/openmob/mobileimsdk/server/demo/ServerEventListenerImpl.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2015 Jack Jiang The MobileIMSDK Project. 
+ * All rights reserved.
+ * Project URL:https://github.com/JackJiang2011/MobileIMSDK
+ *  
+ * openmob.net PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 
+ * ServerEventListenerImpl.java at 2015-11-26 12:00:13, code by Jack Jiang.
+ * You can contact author with jack.jiang@openmob.net or jb2011@163.com.
+ */
 package net.openmob.mobileimsdk.server.demo;
 
 import net.openmob.mobileimsdk.server.event.ServerEventListener;
