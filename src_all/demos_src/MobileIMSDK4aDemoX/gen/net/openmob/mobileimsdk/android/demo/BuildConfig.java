4c613f3dcbaf9ecc5d07ef493c62f6be0f28c3e3
JackJiang <jb2011@163.com>
2018-03-19 21:45:54 +0800

    Android工程迁移至Android Studio 3.0

--- src_all/demos_src/MobileIMSDK4aDemoX/gen/net/openmob/mobileimsdk/android/demo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.openmob.mobileimsdk.android.demo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src_all/libs_src/MobileIMSDK4aXOpen/gen/net/openmob/mobileimsdk/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.openmob.mobileimsdk.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
