ebaf0f59af3d0b89728ad68c9a7de38342e6969f
Jack Jiang <jack.jiang@52im.net>
2021-07-01 15:11:01 +0800

    v6.0开发中，Andriod端TCP版SDK升级已完工

--- sdk_src/TCP_Client/MobileIMSDK4a_tcp_Open/app/src/main/java/net/x52im/mobileimsdk/android/utils/TCPUtils.java
+++ sdk_src/TCP_Client/MobileIMSDK4a_tcp_Open/app/src/main/java/net/x52im/mobileimsdk/android/utils/TCPUtils.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2020  即时通讯网(52im.net) & Jack Jiang.
- * The MobileIMSDK_TCP (MobileIMSDK v5.x TCP版) Project.
+ * Copyright (C) 2021  即时通讯网(52im.net) & Jack Jiang.
+ * The MobileIMSDK_TCP (MobileIMSDK v6.x TCP版) Project. 
  * All rights reserved.
- *
+ * 
  * > Github地址：https://github.com/JackJiang2011/MobileIMSDK
  * > 文档地址：  http://www.52im.net/forum-89-1.html
  * > 技术社区：  http://www.52im.net/
- * > 技术交流群：320837163 (http://www.52im.net/topic-qqgroup.html)
+ * > 技术交流群：215477170 (http://www.52im.net/topic-qqgroup.html)
  * > 作者公众号：“即时通讯技术圈】”，欢迎关注！
  * > 联系作者：  http://www.52im.net/thread-2792-1-1.html
- *
+ *  
  * "即时通讯网(52im.net) - 即时通讯开发者社区!" 推荐开源工程。
- *
- * TCPUtils.java at 2020-8-8 15:58:02, code by Jack Jiang.
+ * 
+ * TCPUtils.java at 2021-7-1 15:08:17, code by Jack Jiang.
  */
 package net.x52im.mobileimsdk.android.utils;
 
