25768338935dda9b01748e7b823b0c8fac548ae8
Jack Jiang <jack.jiang@52im.net>
2021-07-02 23:02:41 +0800

    v6.0开发中，andriod端udp版SDK代码中的copyright声明更新

--- sdk_src/UDP_Client/MobileIMSDK4a_udp_Open/app/src/main/java/net/x52im/mobileimsdk/android/core/QoS4ReciveDaemon.java
+++ sdk_src/UDP_Client/MobileIMSDK4a_udp_Open/app/src/main/java/net/x52im/mobileimsdk/android/core/QoS4ReciveDaemon.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2020  即时通讯网(52im.net) & Jack Jiang.
- * The MobileIMSDK_UDP (MobileIMSDK v5.x UDP版) Project.
+ * Copyright (C) 2021  即时通讯网(52im.net) & Jack Jiang.
+ * The MobileIMSDK_UDP (MobileIMSDK v6.x UDP版) Project. 
  * All rights reserved.
- *
+ * 
  * > Github地址：https://github.com/JackJiang2011/MobileIMSDK
  * > 文档地址：  http://www.52im.net/forum-89-1.html
  * > 技术社区：  http://www.52im.net/
- * > 技术交流群：320837163 (http://www.52im.net/topic-qqgroup.html)
+ * > 技术交流群：215477170 (http://www.52im.net/topic-qqgroup.html)
  * > 作者公众号：“即时通讯技术圈】”，欢迎关注！
  * > 联系作者：  http://www.52im.net/thread-2792-1-1.html
- *
+ *  
  * "即时通讯网(52im.net) - 即时通讯开发者社区!" 推荐开源工程。
- *
- * QoS4ReciveDaemon.java at 2020-8-18 15:40:03, code by Jack Jiang.
+ * 
+ * QoS4ReciveDaemon.java at 2021-7-2 22:38:44, code by Jack Jiang.
  */
 package net.x52im.mobileimsdk.android.core;
 
