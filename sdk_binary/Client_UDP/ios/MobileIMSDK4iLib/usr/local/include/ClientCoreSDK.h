c6da7f940a82f2237678228cc4a5c476803b1f61
Jack Jiang <jack.jiang@52im.net>
2021-07-17 10:53:54 +0800

    v6.0开发中，日常提交

--- sdk_binary/Client_UDP/ios/MobileIMSDK4iLib/usr/local/include/ClientCoreSDK.h
+++ sdk_binary/Client_UDP/ios/MobileIMSDK4iLib/usr/local/include/ClientCoreSDK.h
@@ -1,12 +1,12 @@
 //  ----------------------------------------------------------------------
-//  Copyright (C) 2020  即时通讯网(52im.net) & Jack Jiang.
-//  The MobileIMSDK_X (MobileIMSDK v4.x) Project.
+//  Copyright (C) 2021  即时通讯网(52im.net) & Jack Jiang.
+//  The MobileIMSDK_UDP (MobileIMSDK v6.x UDP版) Project.
 //  All rights reserved.
 //
 //  > Github地址: https://github.com/JackJiang2011/MobileIMSDK
 //  > 文档地址:    http://www.52im.net/forum-89-1.html
 //  > 技术社区：   http://www.52im.net/
-//  > 技术交流群： 320837163 (http://www.52im.net/topic-qqgroup.html)
+//  > 技术交流群： 215477170 (http://www.52im.net/topic-qqgroup.html)
 //  > 作者公众号： “即时通讯技术圈】”，欢迎关注！
 //  > 联系作者：   http://www.52im.net/thread-2792-1-1.html
 //
@@ -19,19 +19,23 @@
 #import "ChatBaseEvent.h"
 #import "MessageQoSEvent.h"
 #import "CompletionDefine.h"
+#import "PLoginInfo.h"
 
 @interface ClientCoreSDK : NSObject
 
 //@property (nonatomic, assign) BOOL localDeviceNetworkOk;
 @property (nonatomic, assign) BOOL connectedToServer;
 @property (nonatomic, assign) BOOL loginHasInit;
-@property (nonatomic, retain) NSString *currentLoginUserId;
-@property (nonatomic, retain) NSString *currentLoginToken;
-@property (nonatomic, retain) NSString *currentLoginExtra;
+@property (nonatomic, retain) PLoginInfo *currentLoginInfo;
+
 
 @property (nonatomic, retain) id<ChatMessageEvent> chatMessageEvent;
 @property (nonatomic, retain) id<ChatBaseEvent> chatBaseEvent;
 @property (nonatomic, retain) id<MessageQoSEvent> messageQoSEvent;
+
+
+//------------------------------------------------------
+#pragma mark - 静态方法
 
 + (ClientCoreSDK *)sharedInstance;
 + (BOOL) isENABLED_DEBUG;
@@ -39,8 +43,14 @@
 + (BOOL) isAutoReLogin;
 + (void) setAutoReLogin:(BOOL)arl;
 
+
+//------------------------------------------------------
+#pragma mark - 实例方法
+
 - (BOOL) isInitialed;
 - (void)initCore;
 - (void) releaseCore;
+- (void) saveFirstLoginTime:(long)firstLoginTime;
+- (NSString *) currentLoginUserId;
 
 @end
--- sdk_binary/Client_UDP/ios/MobileIMSDK4iLib/usr/local/include/ProtocalFactory.h
+++ sdk_binary/Client_UDP/ios/MobileIMSDK4iLib/usr/local/include/ProtocalFactory.h
@@ -1,12 +1,12 @@
 //  ----------------------------------------------------------------------
-//  Copyright (C) 2020  即时通讯网(52im.net) & Jack Jiang.
-//  The MobileIMSDK_X (MobileIMSDK v4.x) Project.
+//  Copyright (C) 2021  即时通讯网(52im.net) & Jack Jiang.
+//  The MobileIMSDK_UDP (MobileIMSDK v6.x UDP版) Project.
 //  All rights reserved.
 //
 //  > Github地址: https://github.com/JackJiang2011/MobileIMSDK
 //  > 文档地址:    http://www.52im.net/forum-89-1.html
 //  > 技术社区：   http://www.52im.net/
-//  > 技术交流群： 320837163 (http://www.52im.net/topic-qqgroup.html)
+//  > 技术交流群： 215477170 (http://www.52im.net/topic-qqgroup.html)
 //  > 作者公众号： “即时通讯技术圈】”，欢迎关注！
 //  > 联系作者：   http://www.52im.net/thread-2792-1-1.html
 //
@@ -17,8 +17,15 @@
 #import "Protocal.h"
 #import "PLoginInfoResponse.h"
 #import "PErrorResponse.h"
+#import "PLoginInfo.h"
+#import "PKickoutInfo.h"
 
 @interface ProtocalFactory : NSObject
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - 协议解析相关方法
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 + (id) parse:(NSData *)fullProtocalJASOnBytes;
 + (id) parse:(NSData *)fullProtocalJASOnBytes withClass:(Class)clazz;
@@ -26,15 +33,23 @@
 + (PLoginInfoResponse *) parsePLoginInfoResponse:(NSString *)dataContentOfProtocal;
 + (PErrorResponse *) parsePErrorResponse:(NSString *) dataContentOfProtocal;
 
++ (PKickoutInfo *)parsePKickoutInfo:(NSString *)dataContentOfProtocal;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - 协议组装相关方法
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 + (Protocal *) createPLoginoutInfo:(NSString *) user_id;
-+ (Protocal *) createPLoginInfo:(NSString *)loginUserId withToken:(NSString *)loginToken andExtra:(NSString *)extra;
++ (Protocal *) createPLoginInfo:(PLoginInfo *)loginInfo;
 + (Protocal *) createPKeepAlive:(NSString *)from_user_id;
 + (Protocal *) createCommonData:(NSString *)dataContent fromUserId:(NSString *)from_user_id toUserId:(NSString *)to_user_id;
 + (Protocal *) createCommonData:(NSString *)dataContent fromUserId:(NSString *)from_user_id toUserId:(NSString *)to_user_id withTypeu:(int)typeu;
 + (Protocal *) createCommonData:(NSString *)dataContent fromUserId:(NSString *)from_user_id toUserId:(NSString *)to_user_id qos:(bool)QoS fp:(NSString *)fingerPrint withTypeu:(int)typeu;
 + (Protocal *) createRecivedBack:(NSString *)from_user_id toUserId:(NSString *)to_user_id withFingerPrint:(NSString *)recievedMessageFingerPrint;
 + (Protocal *) createRecivedBack:(NSString *)from_user_id toUserId:(NSString *)to_user_id withFingerPrint:(NSString *)recievedMessageFingerPrint andBridge:(bool)bridge;
+
++ (Protocal *) createPKickout:(nonnull NSString *)to_user_id code:(int)code reason:(nullable NSString *)reason;
 
 @end
 
