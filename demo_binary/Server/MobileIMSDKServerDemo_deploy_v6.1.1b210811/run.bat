04f74d508b8e430df6e66a532c43535f211dddda
Jack Jiang <jack.jiang@52im.net>
2021-08-11 23:34:42 +0800

    v6.1.1开发中，服务端Demo 2进制版提交

--- demo_binary/Server/MobileIMSDKServerDemo_deploy_v6.1.1b210811/run.bat
+++ demo_binary/Server/MobileIMSDKServerDemo_deploy_v6.1.1b210811/run.bat
@@ -1,8 +1,8 @@
 @echo off
 
-@echo 欢迎使用 MobileIMSDK v6.1 服务端 (build210723)
+@echo 欢迎使用 MobileIMSDK v6.1.1 服务端 (build210811)
 @echo.
-@echo 基于MobileIMSDK的IM服务器正在运行中...
+@echo 基于MobileIMSDK的IM服务器正在运行中 ...
 @echo.
 
 "%JAVA_HOME%/bin/java" -cp lib/gson-2.8.6.jar;lib/log4j-api-2.12.1.jar;lib/log4j-core-2.12.1.jar;lib/log4j-slf4j-impl-2.13.3.jar;lib/slf4j-api-1.7.30.jar;lib/MobileIMSDKServer.jar;lib/rabbitmq-client.jar;"%JAVA_HOME%/lib/tools.jar;classes/.;lib/netty-all-4.1.50.Final.jar;" net.x52im.mobileimsdk.server.demo.ServerLauncherImpl %1
