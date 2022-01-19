fe9fe37fe3260949fc421c8fbf6fa273c40dbd7b
Jack Jiang <jb2011@163.com>
2022-01-19 20:21:51 +0800

    v6.2beta提交，服务端Demo可执行文件更新

--- demo_binary/Server/MobileIMSDKServerDemo_deploy_v6.1.2b211214/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo $JAVA_HOME/bin/java -cp lib/gson-2.8.6.jar:lib/log4j-api-2.15.015.0.jar:lib/log4j-core-2.15.0.jar:lib/log4j-slf4j-impl-2.15.0.jar:lib/slf4j-api-1.7.30.jar:lib/MobileIMSDKServer.jar:lib/rabbitmq-client.jar:$JAVA_HOME/lib/tools.jar:classes/.:lib/netty-all-4.1.50.Final.jar: net.x52im.mobileimsdk.server.demo.ServerLauncherImpl %1
