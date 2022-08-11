029e48b20b41704eb83214607d72edfc75ac11fb
大坏蛋2 <109294571+bigbadboyo@users.noreply.github.com>
2022-08-11 17:21:11 +0800

    常规更新

--- sdk_src/TCP_Client/MobileIMSDK4a_tcp_Open/app/src/main/java/net/x52im/mobileimsdk/android/core/KeepAliveDaemon.java
+++ sdk_src/TCP_Client/MobileIMSDK4a_tcp_Open/app/src/main/java/net/x52im/mobileimsdk/android/core/KeepAliveDaemon.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2021  即时通讯网(52im.net) & Jack Jiang.
- * The MobileIMSDK_TCP (MobileIMSDK v6.x TCP版) Project.
+ * Copyright (C) 2022  即时通讯网(52im.net) & Jack Jiang.
+ * The MobileIMSDK_TCP (MobileIMSDK v6.x TCP版) Project. 
  * All rights reserved.
- *
+ * 
  * > Github地址：https://github.com/JackJiang2011/MobileIMSDK
  * > 文档地址：  http://www.52im.net/forum-89-1.html
  * > 技术社区：  http://www.52im.net/
- * > 技术交流群：215477170 (http://www.52im.net/topic-qqgroup.html)
+ * > 技术交流群：185926912 (http://www.52im.net/topic-qqgroup.html)
  * > 作者公众号：“即时通讯技术圈】”，欢迎关注！
  * > 联系作者：  http://www.52im.net/thread-2792-1-1.html
- *
+ *  
  * "即时通讯网(52im.net) - 即时通讯开发者社区!" 推荐开源工程。
- *
- * KeepAliveDaemon.java at 2021-7-2 15:58:06, code by Jack Jiang.
+ * 
+ * KeepAliveDaemon.java at 2022-7-28 17:24:47, code by Jack Jiang.
  */
 package net.x52im.mobileimsdk.android.core;
 
