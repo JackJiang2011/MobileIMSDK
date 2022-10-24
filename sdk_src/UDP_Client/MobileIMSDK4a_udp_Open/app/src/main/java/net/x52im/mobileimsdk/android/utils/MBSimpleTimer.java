50651bc083746b32ac13e1388057088291d538b6
大坏蛋2 <109294571+bigbadboyo@users.noreply.github.com>
2022-10-24 10:19:18 +0800

    常规更新

--- sdk_src/UDP_Client/MobileIMSDK4a_udp_Open/app/src/main/java/net/x52im/mobileimsdk/android/utils/MBSimpleTimer.java
+++ sdk_src/UDP_Client/MobileIMSDK4a_udp_Open/app/src/main/java/net/x52im/mobileimsdk/android/utils/MBSimpleTimer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022  即时通讯网(52im.net) & Jack Jiang.
+ * The MobileIMSDK_UDP (MobileIMSDK v6.x UDP版) Project. 
+ * All rights reserved.
+ * 
+ * > Github地址：https://github.com/JackJiang2011/MobileIMSDK
+ * > 文档地址：  http://www.52im.net/forum-89-1.html
+ * > 技术社区：  http://www.52im.net/
+ * > 技术交流群：185926912 (http://www.52im.net/topic-qqgroup.html)
+ * > 作者公众号：“即时通讯技术圈】”，欢迎关注！
+ * > 联系作者：  http://www.52im.net/thread-2792-1-1.html
+ *  
+ * "即时通讯网(52im.net) - 即时通讯开发者社区!" 推荐开源工程。
+ * 
+ * MBSimpleTimer.java at 2022-7-28 17:23:39, code by Jack Jiang.
+ */
 package net.x52im.mobileimsdk.android.utils;
 
 import android.os.Handler;
