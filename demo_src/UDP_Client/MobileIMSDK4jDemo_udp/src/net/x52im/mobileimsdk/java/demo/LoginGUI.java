f0e946b7f6578e60142826cec64d973768d3bf15
Jack Jiang <jb2011@163.com>
2023-02-06 12:47:22 +0800

    v6.3提交

--- demo_src/UDP_Client/MobileIMSDK4jDemo_udp/src/net/x52im/mobileimsdk/java/demo/LoginGUI.java
+++ demo_src/UDP_Client/MobileIMSDK4jDemo_udp/src/net/x52im/mobileimsdk/java/demo/LoginGUI.java
@@ -115,7 +115,7 @@ public class LoginGUI extends JFrame
 		mainPanel.addTitledLineSeparator("");
 		JPanel btnAndVerPanel = new JPanel();
 		btnAndVerPanel.setLayout(new BoxLayout(btnAndVerPanel, BoxLayout.LINE_AXIS));
-		JLabel lbVer= new JLabel("v6.2b220716.1");
+		JLabel lbVer= new JLabel("v6.3b221227.1");
 		lbVer.setForeground(new Color(184,184,184));
 		btnAndVerPanel.add(lbVer);
 		btnAndVerPanel.add(Box.createHorizontalGlue());
