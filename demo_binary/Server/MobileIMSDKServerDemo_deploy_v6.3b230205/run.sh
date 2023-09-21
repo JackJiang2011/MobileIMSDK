7c1c8dbc887b715f3e743faff90bf194e39318d4
Jack Jiang <jb2011@163.com>
2023-09-21 21:00:12 +0800

    v6.4提交

--- demo_binary/Server/MobileIMSDKServerDemo_deploy_v6.3b230205/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sudo $JAVA_HOME/bin/java -cp lib/gson-2.8.6.jar:lib/log4j-api-2.17.0.jar:lib/log4j-core-2.17.0.jar:lib/log4j-slf4j-impl-2.17.0.jar:lib/slf4j-api-1.7.30.jar:lib/MobileIMSDKServer.jar:lib/rabbitmq-client.jar:$JAVA_HOME/lib/tools.jar:classes/.:lib/netty-all-4.1.50.Final.jar: net.x52im.mobileimsdk.server.demo.ServerLauncherImpl %1
